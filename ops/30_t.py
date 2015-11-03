b50ded21d296eaa9e18bcbac9aaecdcebc419ec4
陶冶 <taoye@e.hunantv.com>
2015-11-03 09:59:27 +0800

    test

---
+++ ops/30_t.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import time
+import socket
+import json
+rl=[]
+st=int(time.time())
+va=st+100
+hn=socket.gethostname()
+result={ 'endpoint' : hn,
+         'tags' : '',
+         'timestamp' : st,
+         'metric' : 'sys.ops.test',
+         'value' : va,
+         'counterType' : 'GAUGE',
+         'step' : 30
+       }
+#one two
+rl.append(json.dumps(result))
+print rl
